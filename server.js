89000df564e97ad77f0b768ae0147fc7aa946d54
root <root@li549-109.members.linode.com>
2013-02-27 02:05:43 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Nlog
+A node.js blog by Jared De Blander.
+
+Copyright (C) 2013 Jared De Blander
+jared0x90@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
+// Require libs
+var express = require('express');
+var ejs = require('ejs');
+
+// Define variables
+var port = 80;
+
+// Start Express
+var app = express();
+
+// Test express
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(port);
+console.log('Listening on port ' + port);
+
+// app.listen(80);	
